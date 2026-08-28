cf8711e28f58da9be586413bf51363a2548eff22
nz366 <180265222+nz366@users.noreply.github.com>
2026-08-28 13:32:50 +0000

    chore: update test

--- test/forms.dart
+++ test/forms.dart
@@ -1,18 +1,21 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:zero_browser/model/model.dart';
 import 'package:zero_browser/providers/history_provider.dart';
 
 void main() {
-  final testQuery = FormSection(
-    title: "query_title",
-    fields: {"search": TextField(name: "search", label: "Search")},
-  );
+  test('Form section query parameters test', () {
+    final testQuery = FormSection(
+      title: "query_title",
+      fields: {"search": TextField(name: "search", label: "Search")},
+    );
 
-  testQuery.fields["search"]?.value = "Hello World!";
+    testQuery.fields["search"]?.value = "Hello World!";
 
-  final uri = newFormUri(Uri.parse("https://example.com"), testQuery);
+    final uri = newFormUri(Uri.parse("https://example.com"), testQuery);
 
-  assert(uri.queryParameters.containsKey("search"));
+    expect(uri.queryParameters.containsKey("search"), isTrue);
 
-  final result = Uri.decodeComponent(uri.queryParameters["search"]!);
-  assert(result == "Hello World!");
+    final result = Uri.decodeComponent(uri.queryParameters["search"]!);
+    expect(result, "Hello World!");
+  });
 }
